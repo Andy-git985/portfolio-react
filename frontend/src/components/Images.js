38603a7ff7753715476508781a579c6e86bc0336
Andy-git985 <andy.yu617@gmail.com>
2022-11-16 19:36:23 -0500

    material ui masonry

--- frontend/src/components/Images.js
+++ frontend/src/components/Images.js
@@ -1,20 +1,33 @@
+import { ImageList, ImageListItem } from '@mui/material';
+
 const Image = ({ image }) => {
   return (
     <div>
-      <img src={image.image} alt="" />
+      <img src={image.image} alt={image.title} loading="lazy" />
     </div>
   );
 };
 
 const Images = ({ images }) => {
   return (
-    <div className="images">
+    <ImageList variant="masonry" gap={8} className="images">
       {images
         .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
         .map((image) => (
-          <Image key={image.id} image={image} />
+          <ImageListItem key={image.id}>
+            <Image image={image} />
+          </ImageListItem>
         ))}
-    </div>
+    </ImageList>
+
+    // original html
+    // <div className="images">
+    //   {images
+    //     .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    //     .map((image) => (
+    //       <Image key={image.id} image={image} />
+    //     ))}
+    // </div>
   );
 };
 
