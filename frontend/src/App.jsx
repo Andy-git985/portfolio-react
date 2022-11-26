3e99b05edea0447a500629608d45e8f7b7995ce4
Andy-git985 <andy.yu617@gmail.com>
2022-11-26 18:00:53 -0500

    Image component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
 import { initializePosts } from './reducers/postReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import postService from './services/posts';
+import Images from './components/Images';
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(initializePosts());
   }, [dispatch]);
-  const images = useSelector(({ posts }) => {
-    return posts;
-  });
 
   return (
     <div>
-      {images.map((image) => {
-        return (
-          <div key={image.id}>
-            <img src={image.image} alt={image.title}></img>
-          </div>
-        );
-      })}
+      <Images />
     </div>
   );
 };
--- frontend/src/components/Images.jsx
+++ frontend/src/components/Images.jsx
@@ -1,62 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { removePost } from '../reducers/postReducer';
-import { Link } from 'react-router-dom';
-import { ImageList, ImageListItem } from '@mui/material';
-import postService from '../services/posts';
-import LoginButton from './LoginButton';
-
-const Image = ({ image }) => {
-  return (
-    <div>
-      <Link to={`/${image.id}`}>
-        {' '}
-        <img src={image.image} alt={image.title} loading="lazy" />
-      </Link>
-    </div>
-  );
-};
-
+import { useSelector } from 'react-redux';
 const Images = () => {
-  const dispatch = useDispatch();
-
   const images = useSelector(({ posts }) => {
     return posts;
   });
-  console.log(images);
-  const handleClick = (id) => {
-    dispatch(removePost(id));
-  };
-
   return (
-    <>
-      {images.length > 1 ? (
-        <ImageList className="images masonry" variant="masonry" gap={8}>
-          {images.map((image) => (
-            <ImageListItem key={image.id}>
-              <Image image={image} />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      ) : (
-        <div className="images">
-          {images.map((image) => (
-            <div key={image.id}>
-              <Image image={image} />
-              <button onClick={() => handleClick(image.id)}>Delete</button>
-            </div>
-          ))}
-        </div>
-      )}
-    </>
-
-    // original html
-    // <div className="images">
-    //   {images
-    //     .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-    //     .map((image) => (
-    //       <Image key={image.id} image={image} />
-    //     ))}
-    // </div>
+    <div>
+      {images.map((image) => {
+        return (
+          <div key={image.id}>
+            <img src={image.image} alt={image.title}></img>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
